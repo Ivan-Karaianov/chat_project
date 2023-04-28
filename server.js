8a6b04aa74c18b5143b30ec13d7e748ef08a791a
Ivan-Karaianov <ivan.karaianov@sigma.software>
2023-04-28 15:33:52 +0300

    dsfsd

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const app = express();
+const PORT = 3000;
+
+// Enable CORS for both origins
+app.use(cors({
+  origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'localhost:5500', '127.0.0.1:5500']
+}));
+
+app.use(bodyParser.json());
+
+const messages = [];
+app.get('/messages', (req, res) => {
+  const lastIndex = parseInt(req.query.lastIndex, 10) || 0;
+  const newMessages = messages.slice(lastIndex);
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5500');
+  res.json(newMessages);
+});
+
+app.post('/messages', (req, res) => {
+  const newMessage = req.body;
+  messages.push(newMessage);
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5500');
+  res.json({ message: 'Message added successfully' });
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+server.setTimeout(5000);
